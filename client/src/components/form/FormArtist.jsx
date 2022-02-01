1b584811d4933c08b70ae680dbc88d9ffee7aa51
Melodie Shahrvari <mshahrvari@gmail.com>
2022-02-01 18:03:40 +0100

    add and upddate artiste

--- client/src/components/form/FormArtist.jsx
+++ client/src/components/form/FormArtist.jsx
@@ -1,17 +1,137 @@
-// custom tools
-import LabPreview from "../LabPreview";
 // styles
 import "./../../styles/form.css";
 
+import React, { useState, useRef, useEffect } from "react";
+import APIHandler from "../../api/APIHandler";
+import { useParams } from "react-router-dom";
+
 const FormArtist = () => {
-	return (
-		<>
-			<h1 className="title diy">D.I.Y (FormArtist)</h1>
-			<p>Code a form to Create/Update artists.</p>
-			<LabPreview name="artistForm" isSmall />
-			<hr />
-		</>
-	);
+  const [styles, setStyles] = useState([]);
+  const [artist, setArtist] = useState({
+    name: "",
+    description: "",
+    style: "",
+    isBand: true,
+  });
+
+  const { id } = useParams();
+
+  console.log(id);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevent the form to reload
+    try {
+      if (!id) {
+        const { data } = await APIHandler.post("/artists", artist);
+        console.log("artist: ", data);
+      } else {
+        const { data } = await APIHandler.patch(`/artists/${id}`, artist);
+        console.log("single: ", data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(async () => {
+    try {
+      const { data } = await APIHandler.get("/styles");
+      setStyles(data.styles);
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
+
+  useEffect(async () => {
+    const { data } = await APIHandler.get("/artists/" + id);
+    setArtist(data);
+  }, [id]);
+
+  console.log(styles);
+
+  return (
+    <>
+      <h1 className="title diy">Create New Artist</h1>
+      {/* <p>Code a form to Create/Update artists.</p> */}
+      {/* <LabPreview name="artistForm" isSmall /> */}
+      <hr />
+
+      <form className="form">
+        <div>
+          <label htmlFor="nom">nom</label>
+          <input
+            className="input"
+            name="name"
+            type="text"
+            placeholder="name"
+            value={artist.name}
+            onChange={(e) => setArtist({ ...artist, name: e.target.value })}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="description">description</label>
+          <input
+            className="input"
+            name="description"
+            type="text"
+            placeholder="description"
+            value={artist.description}
+            onChange={(e) =>
+              setArtist({ ...artist, description: e.target.value })
+            }
+          />
+        </div>
+
+        <div>
+          <p>style</p>
+          <select
+            name="style"
+            className="input"
+            id="style"
+            value={artist.style}
+            onChange={(e) => setArtist({ ...artist, style: e.target.value })}
+          >
+            <option value=""></option>
+            {styles.map((e) => {
+              return (
+                <option value={e._id} key={e._id}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className="row">
+          <p>Is Band?</p>
+          <label className="label">yes</label>
+          <input
+            name="isBand"
+            id="yes"
+            type="radio"
+            checked
+            value="yes"
+            onChange={(e) => setArtist({ ...artist, isBand: e.target.checked })}
+          />
+          <label className="label">no</label>
+          <input
+            name="isBand"
+            id="no"
+            type="radio"
+            checked
+            value="no"
+            onChange={(e) => setArtist({ ...artist, isBand: e.target.checked })}
+          />
+        </div>
+
+        <button className="button" onClick={handleSubmit}>
+          ok
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default FormArtist;
--- server/routes/artists.js
+++ server/routes/artists.js
@@ -7,7 +7,7 @@ const router = new express.Router();
 const artistModel = require("../models/Artist");
 const albumModel = require("../models/Album");
 
-const getAverageRate = async idArtist => {
+const getAverageRate = async (idArtist) => {
   // use agregate features @ mongo db to code this feature
   // https://docs.mongodb.com/manual/aggregation/
 };
@@ -26,16 +26,17 @@ router.get("/artists", async (req, res, next) => {
     .populate("style")
     .sort(sortQ)
     .limit(limitQ)
-    .then(async artists => {
+    .then(async (artists) => {
       const artistsWithRatesAVG = await Promise.all(
-        artists.map(async res => {
-          // AVG : things are getting tricky here ! :) 
+        artists.map(async (res) => {
+          // AVG : things are getting tricky here ! :)
           // the following map is async, updating each artist with an avg rate
           const copy = res.toJSON(); // copy the artist object (mongoose response are immutable)
           // copy.avg = await getAverageRate(res._id); // get the average rates fr this artist
 
           copy.isFavorite =
-            req.user && req.user.favorites.artists.includes(copy._id.toString());
+            req.user &&
+            req.user.favorites.artists.includes(copy._id.toString());
           return copy; // return to the mapped result array
         })
       );
@@ -45,25 +46,35 @@ router.get("/artists", async (req, res, next) => {
     .catch(next);
 });
 
-
-router.get("/artists/:id", (req, res, next) => {
-  res.status(200).json({ msg: "@todo" })
+router.get("/artists/:id", async (req, res, next) => {
+  try {
+    const singleArtist = await artistModel.findById(req.params.id);
+    res.status(200).json(singleArtist);
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.get("/filtered-artists", (req, res, next) => {
-  res.status(200).json({ msg: "@todo" })
+  res.status(200).json({ msg: "@todo" });
 });
 
-router.post("/artists", (req, res) => {
-  res.status(200).json({ msg: "@todo" })
+router.post("/artists", (req, res, next) => {
+  artistModel
+    .create(req.body)
+    .then((newArtist) => res.status(200).json(newArtist))
+    .catch(next);
 });
 
 router.patch("/artists/:id", async (req, res, next) => {
-  res.status(200).json({ msg: "@todo" })
+  artistModel
+    .findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updateArtist) => res.status(200).json(updateArtist))
+    .catch(next);
 });
 
 router.delete("/artists/:id", (req, res, next) => {
-  res.status(200).json({ msg: "@todo" })
+  res.status(200).json({ msg: "@todo" });
 });
 
 module.exports = router;
